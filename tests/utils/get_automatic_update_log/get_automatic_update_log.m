ddd39b262ea803c9cebade0c75fea23c5bc0be91
unknown <shahnewaz_113@yahoo.com>
2015-12-02 15:50:17 +0600

    How to run document. Along with test folder structure setup.

--- tests/utils/get_automatic_update_log/get_automatic_update_log.m
+++ tests/utils/get_automatic_update_log/get_automatic_update_log.m
@@ -1,5 +1,13 @@
+/*
+	Script Name	: set automatic/frequent updating of datasets & srcs
+	Author		: Md. Shahnewaz Rasel
+	Input	: Folder Path		  
+	Output	: Track all files in that folder
+	GitHub	: #06
+*/
+(Folder_Path as text)=>
 let
-    Source = Folder.Files("C:\Users\Dell\Documents\datasets"),
+    Source = Folder.Files(Folder_Path),
     #"Added Custom" = Table.AddColumn(Source, "File Path", each [Folder Path] & [Name]),
     #"Reordered Columns" = Table.ReorderColumns(#"Added Custom",{"Content", "Name", "Extension", "File Path", "Date accessed", "Date modified", "Date created", "Attributes", "Folder Path"}),
     #"Removed Other Columns" = Table.SelectColumns(#"Reordered Columns",{"File Path", "Date accessed", "Date modified", "Date created"}),
